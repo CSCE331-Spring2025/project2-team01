cd3995bf1126f6ba5cf1087b5d4c775291885c55
Siddesh <siddesh2022@tamu.edu>
2025-02-17 09:09:58 -0600

    added Employee population code

---
+++ populateEmployees.py
@@ -0,0 +1,17 @@
+class Employees:
+    def __init__(Uuid, isManager, Name, PayGrade, Hours):
+        self.Uuid = Uuid
+        self.isManager = isManager
+        self.name = name
+        self.payGrade = payGrade
+        self.hours = hours
+    def getUuid(self):
+        return self.Uuid
+    def getIsManager(self):
+        return self.Name
+    def getName(self):
+        return self.isManager
+    def getPayGrade(self):
+        return self.payGrade
+    def getHours(self):
+        return self.Hours
